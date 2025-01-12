6c2519d19189e6831eb7627d5c11821c94e4ff31
anise0201 <anise0201@gmail.com>
2025-01-12 22:49:28 +0800

    test

--- public_html/index.php
+++ public_html/index.php
@@ -10,7 +10,7 @@ displayToast();
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Kerepek Funz</title>
+    <title>Chipzzia</title>
     <?php head_tag_content(); ?>
   </head>
   <body data-bs-spy="scroll" data-bs-target="#navbar" data-bs-root-margin="0px 0px -40%" data-bs-smooth-scroll="true" tabindex="0" bgcolor=#FF682D>
